fef3b825fac09b904b3f063f03c711923e98ba9c
switch87 <switch87@mx>
2024-06-17 16:27:01 +0200

    google price

--- snakebyte/website_sale_snakebyte/models/product_template.py
+++ snakebyte/website_sale_snakebyte/models/product_template.py
@@ -18,12 +18,19 @@ class ProductTemplate(models.Model):
     out_of_stock_message = fields.Html("Out-of-stock Message", compute=get_out_of_stock_message, store=False)
 
     def _default_website_meta(self):
+        taxes = self.taxes_id
+        price_unit = self.price
+        taxes = taxes.compute_all(price_unit, currency=self.env.user.company_id.currency_id, quantity=1, product=self, partner=None)
+        price_incl_tax = taxes['total_included']
+
+        website = self.env['website'].get_current_website()
+        base_url = website.get_base_url()
         res = super(ProductTemplate, self)._default_website_meta()
         res['default_opengraph']['g:title'] = self.name
         res['default_opengraph']['g:id'] = self.id
         res['default_opengraph']['g:description'] = self.description_sale
-        res['default_opengraph']['g:image'] = self.env['website'].image_url(self, 'image_1024')
-        res['default_opengraph']['g:price'] = self.list_price
+        res['default_opengraph']['g:image'] = base_url+ self.env['website'].image_url(self, 'image_1024')
+        res['default_opengraph']['g:price'] = price_incl_tax
         res['default_opengraph']['g:availability'] = 'in_stock'
         res['default_opengraph']['g:condition'] = 'new'
         return res
